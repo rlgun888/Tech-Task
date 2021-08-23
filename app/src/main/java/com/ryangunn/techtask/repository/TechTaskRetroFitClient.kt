b729c1ba2e908547afe06934c7f58a83af2b7a16
rlgun888 <89319971+rlgun888@users.noreply.github.com>
2021-08-23 03:51:14 -0500

    Added ClientID to network calls

--- app/src/main/java/com/ryangunn/techtask/repository/TechTaskRetroFitClient.kt
+++ app/src/main/java/com/ryangunn/techtask/repository/TechTaskRetroFitClient.kt
@@ -1,6 +1,9 @@
 package com.ryangunn.techtask.repository
 
+import com.ryangunn.techtask.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -8,8 +11,13 @@ object TechTaskRetroFitClient {
 
     private const val BASE_URL = "https://private-f699e3-masteryinterview.apiary-mock.com"
 
-   private fun getRetroFit():Retrofit{
-        val okHttpClient = OkHttpClient.Builder().build()
+   private fun getRetroFit():Retrofit {
+       val builder = OkHttpClient.Builder()
+       val okHttpClient = builder.addInterceptor(Interceptor { chain: Interceptor.Chain ->
+           val request: Request =
+               chain.request().newBuilder().addHeader("clientId", BuildConfig.CLIENT_ID).build()
+           chain.proceed(request)
+       }).build()
        return Retrofit.Builder().baseUrl(BASE_URL)
            .client(okHttpClient)
            .addConverterFactory(GsonConverterFactory.create()).build()
